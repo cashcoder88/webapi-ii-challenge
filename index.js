6a1977c12559e268f8a01eb2c26b18871af2f11f
cashcoder88 <cashglobe@gmail.com>
2019-05-29 12:56:04 -0600

    First get request done

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 
+const db = require('./data/db.js');
+
 const server = express();
+server.use(express.json());
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({
+             error: "The users information could not be retrieved." 
+        });
+    });
+});
 
 server.listen(6000, () => {
     console.log('Listening On Port 6000')
